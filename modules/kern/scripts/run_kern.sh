e093bd198f467009c8cd48ed7d532dd0a79ed6a0
915-Pascu-Victor <victor.pascu@stud.ubbcluj.ro>
2024-07-25 18:59:57 +0300

    chore: Update run script

--- modules/kern/scripts/run_kern.sh
+++ modules/kern/scripts/run_kern.sh
@@ -1,7 +1,7 @@
 set -e
 sh scripts/check_headers.sh && echo "All headers are good"
-make
+make -j16
 sh scripts/make_image.sh
 echo "STARTING BOOT:"
-qemu-system-x86_64 -m 4G -drive file=image.hdd,format=raw -bios /usr/share/OVMF/x64/OVMF.fd -chardev stdio,id=char0,mux=on,logfile=serial.log,signal=off \
+qemu-system-x86_64 -m 256M -drive file=image.hdd,format=raw -bios /usr/share/OVMF/x64/OVMF.fd -chardev stdio,id=char0,mux=on,logfile=serial.log,signal=off \
 -serial chardev:char0 -mon chardev=char0 -s -S -d guest_errors -D qemu.log -no-reboot
