b216f180e810d852526e9cea04666cab86e986b2
Pascu-Victor <me@vpascu.ro>
2025-12-02 15:37:35 +0200

    place debug messages for exit under EXIT_DEBUG

--- modules/kern/src/syscalls_impl/process/exit.cpp
+++ modules/kern/src/syscalls_impl/process/exit.cpp
@@ -16,7 +16,9 @@ void wos_proc_exit(int status) {
         return;
     }
 
+#ifdef EXIT_DEBUG
     ker::mod::dbg::log("wos_proc_exit: Task PID %x exiting with status %d", currentTask->pid, status);
+#endif
 
     // Store exit status for waiting processes
     currentTask->exitStatus = status;
@@ -25,7 +27,9 @@ void wos_proc_exit(int status) {
     // Reschedule all tasks waiting for this process to exit
     for (uint64_t i = 0; i < currentTask->awaitee_on_exit_count; ++i) {
         uint64_t waitingPid = currentTask->awaitee_on_exit[i];
+#ifdef EXIT_DEBUG
         ker::mod::dbg::log("wos_proc_exit: Rescheduling waiting task PID %x", waitingPid);
+#endif
 
         auto* waitingTask = ker::mod::sched::findTaskByPid(waitingPid);
         if (waitingTask != nullptr) {
@@ -41,14 +45,20 @@ void wos_proc_exit(int status) {
                 // Get kernel-accessible pointer via HHDM offset
                 auto* statusPtr = reinterpret_cast<int32_t*>(ker::mod::mm::addr::getVirtPointer(waitingTask->waitStatusPhysAddr));
                 *statusPtr = status;
+#ifdef EXIT_DEBUG
                 ker::mod::dbg::log("wos_proc_exit: Set exit status %d for waiting task PID %x", status, waitingPid);
+#endif
             }
 
             // Reschedule the waiting task on its original CPU
             ker::mod::sched::rescheduleTaskForCpu(waitingTask->cpu, waitingTask);
+#ifdef EXIT_DEBUG
             ker::mod::dbg::log("wos_proc_exit: Successfully rescheduled waiting task PID %x on CPU %d", waitingPid, waitingTask->cpu);
+#endif
         } else {
+#ifdef EXIT_DEBUG
             ker::mod::dbg::log("wos_proc_exit: Could not find waiting task PID %x", waitingPid);
+#endif
         }
     }
 
@@ -61,7 +71,9 @@ void wos_proc_exit(int status) {
     }
 
     if (currentTask->elfBuffer != nullptr) {
+#ifdef EXIT_DEBUG
         ker::mod::dbg::log("wos_proc_exit: Freeing ELF buffer of size %d", currentTask->elfBufferSize);
+#endif
         delete[] currentTask->elfBuffer;
         currentTask->elfBuffer = nullptr;
         currentTask->elfBufferSize = 0;
@@ -74,9 +86,10 @@ void wos_proc_exit(int status) {
     // - Page tables
     // - Signal handlers
     // TODO: Signal handling:
-    // - Notify parent process (SIGCHLD)
 
+#ifdef EXIT_DEBUG
     ker::mod::dbg::log("wos_proc_exit: Removing task from runqueue");
+#endif
 
     // This function will not return
     jump_to_next_task_no_save();
